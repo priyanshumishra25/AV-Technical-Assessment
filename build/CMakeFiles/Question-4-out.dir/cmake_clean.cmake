b8b1462998da2db24a5175a91feb45f736f166de
Priyanshu <z5518412@ad.unsw.edu.au>
2025-03-10 23:36:09 +1100

    Completed code

---
+++ build/CMakeFiles/Question-4-out.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Question-4-out.dir/Question-4/Question-4.cc.o"
+  "CMakeFiles/Question-4-out.dir/Question-4/Question-4.cc.o.d"
+  "Question-4-out"
+  "Question-4-out.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Question-4-out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,15 @@
+# CMake generated Testfile for 
+# Source directory: /home/priyanshu/Redback/AV-Technical-Assessment
+# Build directory: /home/priyanshu/Redback/AV-Technical-Assessment/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(question-1 "Question-1-out")
+set_tests_properties(question-1 PROPERTIES  _BACKTRACE_TRIPLES "/home/priyanshu/Redback/AV-Technical-Assessment/CMakeLists.txt;13;add_test;/home/priyanshu/Redback/AV-Technical-Assessment/CMakeLists.txt;0;")
+add_test(question-2 "Question-2-out")
+set_tests_properties(question-2 PROPERTIES  _BACKTRACE_TRIPLES "/home/priyanshu/Redback/AV-Technical-Assessment/CMakeLists.txt;13;add_test;/home/priyanshu/Redback/AV-Technical-Assessment/CMakeLists.txt;0;")
+add_test(question-3 "Question-3-out")
+set_tests_properties(question-3 PROPERTIES  _BACKTRACE_TRIPLES "/home/priyanshu/Redback/AV-Technical-Assessment/CMakeLists.txt;13;add_test;/home/priyanshu/Redback/AV-Technical-Assessment/CMakeLists.txt;0;")
+add_test(question-4 "Question-4-out")
+set_tests_properties(question-4 PROPERTIES  _BACKTRACE_TRIPLES "/home/priyanshu/Redback/AV-Technical-Assessment/CMakeLists.txt;45;add_test;/home/priyanshu/Redback/AV-Technical-Assessment/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
